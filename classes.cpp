858ab94e86defceef04598fa757df4d4bd90b700
Kalina <k.dimitrova08@gmail.com>
2025-02-04 19:16:58 +0000

    pyrva promqna

---
+++ classes.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+#include <random>
+#include <windows.h>
+#include <stdlib.h>
+using namespace std;
+
+class Area{
+public:
+    int x, y;
+    int x1, y1;
+
+    int RandomNumber(int area){
+        random_device rd;
+        uniform_int_distribution<int> dist(1, area);
+        return dist(rd);
+    }
+};
+class Player{
+public:
+    int x = 1, y = 1;
+    int score;
+    int steps;
+};
+class Trail{
+public:
+    int n = 0;
+    int arr[1000];
+};
+
+bool map(Player &player, Area &area, Trail &trail) {
+    bool IsItOn = true;
+    trail.n = 2*player.score;
+    int temp = player.score;
+
+
+
+    if (player.x > area.x || player.y > area.y || player.x == 0 || player.y == 0)
+    {
+        cout << "You lost!";
+        cout << endl << "Your steps: " << player.steps;
+        cout << endl << "Your score: " << player.score;
+        return false;
+    }
+
+    area.y = area.y + 2;
+    area.x = area.x + 2;
+    string arr[area.y][area.x];
+
+    for (int i = 0; i < area.y; i++) {
+        for (int j = 0; j < area.x; j++) {
+            if (j == 0 || j == area.x - 1 || i == 0 || i == area.y - 1) {
+                arr[i][j] = "#";
+            }
+            else if (i == player.x && j == player.y) {
+                arr[i][j] = "x";
+                trail.arr[trail.n] = i;
+                trail.arr[trail.n+1] = j;
+            }
+            else if(i == area.x1 && j == area.y1){
+                arr[i][j] = "o";
+                IsItOn = false;
+            }
+            else {
+                arr[i][j] = ".";
+            }
+        }
+    }
+    while(temp > 0){
+        trail.n = 2 * temp;
+        arr[trail.arr[trail.n]][trail.arr[(trail.n + 1)]] = "x";
+        temp--;
+    }
+
+    for (int i = 0; i < area.y; i++)
+    {
+        for (int j = 0; j < area.x; j++)
+        {
+            cout << arr[i][j];
+            cout << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    //cout << trail.arr[trail.n] << endl << trail.arr[trail.n + 1] << endl;
+
+    area.y = area.y - 2;
+    area.x = area.x - 2;
+    if(IsItOn){
+        area.x1 = area.RandomNumber(area.x);
+        area.y1 = area.RandomNumber(area.y);
+        player.score++;
+    }
+    return true;
+}
+
+int Input(Player &player, Area &area, Trail &trail) {
+    bool playing = true;
+    while (playing) {
+        if (GetAsyncKeyState('W') & 0b1) {
+            player.x = player.x - 1;
+            player.steps++;
+            playing = map(player, area, trail);
+        }
+        else if (GetAsyncKeyState('A') & 0b1) {
+            player.y = player.y - 1;
+            player.steps++;
+            playing = map(player, area, trail);
+        }
+        else if (GetAsyncKeyState('S') & 0b1) {
+            player.x = player.x + 1;
+            player.steps++;
+            playing = map(player, area, trail);
+        }
+        else if (GetAsyncKeyState('D') & 0b1) {
+            player.y = player.y + 1;
+            player.steps++;
+            playing = map(player, area, trail);
+        }
+        else if(GetAsyncKeyState('E') & 0b1){
+            return 0;
+        }
+        Sleep(100);
+    }
+    return 0;
+}
+
+int main() {
+    Area area;
+    Player player;
+    Trail trail;
+
+    cout << "Please write the maps size(x, y): ";
+    cin >> area.x >> area.y;
+    area.x1 = area.RandomNumber(area.x);
+    area.y1 = area.RandomNumber(area.y);
+    player.score = 0;
+    player.steps = 0;
+
+    map(player, area, trail);
+    Input(player, area, trail);
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <random>
+#include <windows.h>
+#include <stdlib.h>
+using namespace std;
+
+class Area{
+public:
+    int n, m;
+    int x1, y1;
+};
+class Player{
+public:
+    int x, y;
+    int score;
+    int steps;
+};
+
+int RandomNumber(int area){
+    random_device rd;
+    uniform_int_distribution<int> dist(1, area);
+    return dist(rd);
+}
+
+bool map(int n, int m, int x, int y, int& x1, int& y1, int steps, int& score) {
+    bool IsItOn = true;
+
+    if (x > m || y > n || x == 0 || y == 0)
+    {
+        cout << "You lost!";
+        cout << endl
+             << "Your steps: " << steps;
+        cout << endl
+             << "Your score: " << score;
+        return false;
+    }
+
+    n = n + 2;
+    m = m + 2;
+    string arr[n][m];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (j == 0 || j == m - 1 || i == 0 || i == n - 1) {
+                arr[i][j] = "#";
+            }
+            else if (i == x && j == y) {
+                arr[i][j] = "x";
+            }
+            else if(i == x1 && j == y1){
+                arr[i][j] = "o";
+                IsItOn = false;
+            }
+            else {
+                arr[i][j] = ".";
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << arr[i][j];
+            cout << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    if(IsItOn){
+        x1 = RandomNumber(n-2);
+        y1 = RandomNumber(m-2);
+        map(n-2, m-2, x, y, x1, y1, steps, ++score);
+    }
+    return true;
+}
+
+int Input(int x, int y, int n, int m, int x1, int y1, int steps, int score) {
+    bool playing = true;
+    while (playing) {
+        if (GetAsyncKeyState('W') & 0b1) {
+            x = x - 1;
+            playing = map(n, m, x, y, x1, y1, ++steps, score);
+        }
+        if (GetAsyncKeyState('A') & 0b1) {
+            y = y - 1;
+            playing = map(n, m, x, y, x1, y1, ++steps, score);
+        }
+        if (GetAsyncKeyState('S') & 0b1) {
+            x = x + 1;
+            playing = map(n, m, x, y, x1, y1, ++steps, score);
+        }
+        if (GetAsyncKeyState('D') & 0b1) {
+            y = y + 1;
+            playing = map(n, m, x, y, x1, y1, ++steps, score);
+        }
+        if(GetAsyncKeyState(VK_ESCAPE)){
+            return 0;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    int n, m;
+    cout << "Please write n and m: ";
+    cin >> n >> m;
+    int x = 1, y = 1, steps = 0, score = 0;
+    int x1 = RandomNumber(n);
+    int y1 = RandomNumber(m);
+    map(n, m, x, y, x1, y1, steps, score);
+    Input(x, y, n, m, x1, y1, steps, score);
+    return 0;
+}
